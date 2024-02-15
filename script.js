1bf9af9f0645cf765171b0aa6f594965f3a0a73e
ppsancheztom <tommy151forever@gmail.com>
2024-02-15 14:01:17 -0600

    Actualizacion 15 Feb

---
+++ script.js
@@ -0,0 +1,32 @@
+// let mensaje= null;
+
+function encriptar(){
+    leerMensaje();
+}
+
+function leerMensaje(){
+    let mensaje = document.querySelector('.txtMensaje');
+    let textoMensaje = mensaje.value;
+    
+    if(textoMensaje.trim() === '') {
+        console.log("El mensaje está vacio.");
+        // return;
+    }else{
+
+        let caracteres = textoMensaje.split('');
+        console.log("Caracteres del texto",caracteres);
+        desAvisoIni();
+    }
+}
+
+function desAvisoIni(){
+     // Desactivar elementos con la clase "avisoInicial"
+    var elementosAviso = document.querySelectorAll('.avisoInicial');
+    elementosAviso.forEach(function(elemento) {
+        elemento.style.display='none';
+    });
+
+    document.getElementById('btnCopiar').style.display='block'
+
+    document.getElementById('Mensaje').value='';
+}
